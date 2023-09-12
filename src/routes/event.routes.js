6b853603a73e95219f1c8973d4d26434b9f36dea
Ariel Leyva <aleyva1509@gmail.com>
2023-09-12 13:53:02 -0600

    add: basic event router with no routes

---
+++ src/routes/event.routes.js
@@ -0,0 +1,13 @@
+// Router 
+import { Router } from 'express';
+// Middlewares
+import { authRequired } from '../middlewares/jwtValidate.js';
+
+const router = Router();
+
+// create an event
+
+
+
+
+export default router;
