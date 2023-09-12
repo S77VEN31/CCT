ec820908e978a5d610a5ee17a2018e92463656fc
Ariel Leyva <aleyva1509@gmail.com>
2023-09-12 00:37:34 -0600

    feat: add activities and users to event model

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -1,10 +1,27 @@
 import mongoose from "mongoose";
+import userModel from "./user.model";
+
+const activityModel = new mongoose.Schema({
+    startTime: {
+        type: Date,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+}, { timestamps: true })
 
 const eventModel = new mongoose.Schema({
     title: {
         type: String,
         required: true,
         trim: true,
+    },
+    collaborators: {
+        type: [userModel],
+        required: true,
     },
     description: {
         type: String
@@ -32,13 +49,17 @@ const eventModel = new mongoose.Schema({
         required: true,
     },
     attendees: {
-        type: [String],
+        type: [userModel],
         required: true,
     },
     feedbackForm: {
         type: String,
         required: true,
         trim: true,
-    }
+    },
+    activities: {
+        type: [activityModel],
+        required: true,
+    },
 }, { timestamps: true })
 export default mongoose.model("Event", eventModel);
