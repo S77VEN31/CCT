2a04fcb0e73557aa02b80bd1404021e7c1e721c7
S77VEN31 <stivensg313131@outlook.com>
2023-09-13 13:42:00 -0700

    fix: svgs

--- client/src/enumerables/svgs/svgs.tsx
+++ client/src/enumerables/svgs/svgs.tsx
@@ -2,8 +2,7 @@
 import Logo from "../../assets/images/svgs/Logo";
 import Photo from "../../assets/images/svgs/Photo";
 
-const Svgs = {
+export const Svgs = {
   logo: (props) => <Logo {...props} />,
   photo: (props) => <Photo {...props} />,
 };
-export default Svgs;
