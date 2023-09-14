dba832a609a129227aa8ddfb92405e64c8ebb92d
S77VEN31 <stivensg313131@outlook.com>
2023-09-13 18:34:54 -0700

    add: MenuHeader

--- client/src/components/headers/MenuHeader/MenuHeader.tsx
+++ client/src/components/headers/MenuHeader/MenuHeader.tsx
@@ -5,16 +5,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Icons } from "../../../enumerables/icons/icons";
 // Components
 import NotificationButton from "../../buttons/NotificationButton/NotificationButton";
+import MainProfileCard from "../../cards/MainProfileCard/MainProfileCard";
 
 const MenuHeader = () => {
   return (
     <div className="menu-header">
       <div className="icon-and-title">
-        <div>
-          <FontAwesomeIcon icon={Icons.menu} className="icon" />
-        </div>
+        <FontAwesomeIcon icon={Icons.menu} className="icon" />
         <h2 className="semibold">Búsqueda</h2>
-        <NotificationButton></NotificationButton>
+      </div>
+      <div className="buttons-and-profile">
+        <div className="buttons">
+          <NotificationButton icon={Icons.news} notifications={0} />
+          <NotificationButton icon={Icons.email} notifications={1} />
+        </div>
+        <MainProfileCard image="https://www.elmueble.com/medio/2023/04/14/gato-de-raza-ragdoll_5c5827ec_230414185205_1000x1493.jpg" />
       </div>
     </div>
   );
