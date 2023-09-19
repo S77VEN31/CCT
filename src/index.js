85b0e0bb85e421bdabf0b64d01514039e31a5111
S77VEN31 <stivensg313131@outlook.com>
2023-09-18 18:05:51 -0700

    add: fix port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import app from "./app.js";
 // Connect to DB
 import { connect } from "./db.js";
 // Start server
-app.listen(3000);
+// Define the port to listen on, using the PORT environment variable or default to 3000
+const port = process.env.PORT || 3000;
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 // Connect to DB
 connect();
 // Log message
