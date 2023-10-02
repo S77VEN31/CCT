3c0c9628398b700216ad5f8f28da178760ad93eb
S77VEN31 <stivensg313131@outlook.com>
2023-10-02 10:58:21 -0700

    test body parser

--- src/app.js
+++ src/app.js
@@ -7,12 +7,17 @@ import cookieParser from "cookie-parser";
 // Enable cors
 import cors from "cors";
 
+import bodyParser from 'body-parser';
+
 // AuthRoutes
 import authRoutes from "./routes/auth.routes.js"
 import eventRoutes from "./routes/event.routes.js"
 import userRoutes from "./routes/user.routes.js"
 // Instance of express
 const app = express();
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cors({ origin: 'http://localhost:5173' }))
 // Register middlewares
 app.use(morgan("dev"));
