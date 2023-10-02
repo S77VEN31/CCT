2b1b69a48f012a19c3efe2ffad6c00812257e37c
S77VEN31 <stivensg313131@outlook.com>
2023-10-02 03:48:34 -0700

    add: test file size

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // App entry point
 import express from "express";
+// File upload
+import fileUpload from "express-fileupload";
 // Log requests to console
 import morgan from "morgan";
 // Get cookies from request
@@ -13,6 +15,10 @@ import eventRoutes from "./routes/event.routes.js"
 import userRoutes from "./routes/user.routes.js"
 // Instance of express
 const app = express();
+const uploadOptions = {
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
+};
+app.use(fileUpload(uploadOptions));
 app.use(cors({ origin: 'http://localhost:5173' }))
 // Register middlewares
 app.use(morgan("dev"));
