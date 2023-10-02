50b95cb40435c8a0d252079eb8dc01fb958a128a
S77VEN31 <stivensg313131@outlook.com>
2023-10-02 12:59:00 -0700

    test: users

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,19 +3,18 @@ import User from "../models/user.model.js";
 export const updateProfileInfo = async (req, res) => {
     try {
         const userId = '651a8bfd6df0d6d4c13d48d8';
-        const { name, carne, phone, profilePicture } = req.body;
-        // Encuentra el usuario por su ID
-        const user = await User.findById(userId);
-        // Si no existe el usuario
-        if (!user) {
+        const updateFields = {
+            ...req.body,
+        };
+
+        // Encuentra el usuario por su ID y actualiza los campos proporcionados en req.body
+        const result = await User.updateOne({ _id: userId }, { $set: updateFields });
+
+        if (result.nModified === 0) {
+        // No se actualizó ningún documento, lo que podría indicar que el usuario no existe
             return res.status(404).json({ message: "No existe el usuario." });
         }
-        // Si existe el usuario
-        user.name = name;
-        user.carne = carne;
-        user.phone = phone;
-        user.profilePicture = profilePicture;
-        await user.save();
+
         res.json({ message: "Información actualizada." });
 
     } catch (error) {
