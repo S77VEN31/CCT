4da307720f649592d3c62ec7cdb9d9cef0dafb13
S77VEN31 <stivensg313131@outlook.com>
2023-10-07 19:36:57 -0700

    add: auth required test

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,11 @@
 import { Router } from 'express';
 // Auth Controllers
 import { updateProfileInfo } from '../controllers/users.controller.js';
+// Middlewares
+import { authRequired } from '../middlewares/jwtValidate.middleware.js';
 
 const router = Router();
 
-router.put("/users/updateProfileInfo", updateProfileInfo);
+router.put("/users/updateProfileInfo", authRequired, updateProfileInfo);
 
 export default router;
