44e797f42c9e8fa8daf1bf9f491bef4bc179c58b
S77VEN31 <stivensg313131@outlook.com>
2023-10-12 10:17:36 -0700

    test: 2

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -66,6 +66,7 @@ export const deleteMember = async (req, res) => {
     try {
         const userId = req.user.id;
         const { carne } = req.body;
+        console.log(req.body);
         console.log(carne);
         // find member by carne
         const member = await User.findOne({ carne });
