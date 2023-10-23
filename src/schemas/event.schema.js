7ebdf74eb8228df6d9efc541b704b6e6ac7a651b
S77VEN31 <stivensg313131@outlook.com>
2023-10-22 19:16:14 -0700

    hotfix: change date to string in zod event object validation

--- src/schemas/event.schema.js
+++ src/schemas/event.schema.js
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const eventSchema = z.object({
     title: z.string().min(3).max(255),
     description: z.string().min(3).max(255),
-    startTime: z.date(),
-    endTime: z.date(),
+    startTime: z.string(),
+    endTime: z.string(),
     location: z.string().min(3).max(255),
     capacity: z.number().int().positive(),
     requiredCollaborators: z.number().int().positive(),
