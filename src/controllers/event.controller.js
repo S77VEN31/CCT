5a4482b5d698c9b36d120bf4a863dc2636e8eb5a
S77VEN31 <stivensg313131@outlook.com>
2023-11-05 22:57:21 -0800

    Update getActivitiesFromEvent to return full event object

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -231,7 +231,7 @@ export const createEventActivity = async (req, res) => {
 export const getActivitiesFromEvent = async (req, res) => {
     try {
         const event = await Event.findById(req.body.eventId).populate('activities');
-        res.status(200).json(event.activities);
+        res.status(200).json(event);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
