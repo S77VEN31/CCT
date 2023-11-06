0cbf046b36e85e93fe0bb5f6008346fdaae32eeb
S77VEN31 <stivensg313131@outlook.com>
2023-11-05 16:47:41 -0800

    Add authentication middleware to proposal routes

--- src/routes/proposal.routes.js
+++ src/routes/proposal.routes.js
@@ -2,11 +2,13 @@
 import { Router } from 'express';
 // Data Controllers
 import { getProposals, sendProposal } from "../controllers/proposal.controller.js";
+// Middlewares
+import { authRequired } from '../middlewares/jwtValidate.middleware.js';
 
 const router = Router();
 
 // Event Categories
-router.get('/proposals', getProposals);
+router.get('/proposals', authRequired, getProposals);
 router.post('/proposal/send', sendProposal);
 
 export default router;
